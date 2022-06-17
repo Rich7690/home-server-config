f075ae24a149ce26ccf0d39b3f11727f4363d571
Rich <11451714+Rich7690@users.noreply.github.com>
2022-06-16 23:41:48 -0700

    kube-state-metrics

--- terraform/oauth-proxy.tf
+++ terraform/oauth-proxy.tf
@@ -1,10 +1,3 @@
-resource "kubernetes_service_account_v1" "oauth_proxy" {
-  metadata {
-    name = "oauth-proxy"
-  }
-}
-
-
 resource "kubernetes_secret_v1" "oauth_proxy" {
   metadata {
     name = "oauth-proxy"
@@ -26,29 +19,6 @@ resource "kubernetes_secret_v1" "oauth_proxy" {
     "OAUTH2_PROXY_REDIRECT_URL"      = "https://${var.domain_name}/oauth2/callback"
     "OAUTH2_PROXY_UPSTREAMS"         = "http://jacket:9117/jackett" # doesn't matter what this is, we don't use it
     "OAUTH2_PROXY_COOKIE_SECRET"     = random_password.cookie_secret.result
-  }
-}
-
-
-resource "kubernetes_config_map" "oauth_proxy" {
-  metadata {
-    name = "oauth-proxy"
-    labels = {
-      "app" = "oauth-proxy"
-    }
-  }
-
-  data = {
-    "OAUTH2_PROXY_SCOPE"            = "profile email openid"
-    "OAUTH2_PROXY_HTTP_ADDRESS"     = ":4180"
-    "OAUTH2_PROXY_PROVIDER"         = "keycloak-oidc"
-    "OAUTH2_PROXY_ALLOWED_ROLE"     = "admin"
-    "OAUTH2_PROXY_ALLOWED_GROUPS"   = "/admin"
-    "OAUTH2_PROXY_COOKIE_SAMESITE"  = "lax"
-    "OAUTH2_PROXY_REVERSE_PROXY"    = "true"
-    "OAUTH2_PROXY_SET_XAUTHREQUEST" = "true"
-    "OAUTH2_PROXY_EMAIL_DOMAINS"    = "*"
-    "OAUTH2_PROXY_COOKIE_EXPIRE"    = "336h"
   }
 }
 
