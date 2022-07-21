253ebeca0a70609653878a409c32d73c42d42545
Rich <11451714+Rich7690@users.noreply.github.com>
2022-07-21 16:51:56 -0700

    more fixes

--- terraform/metrics.tf
+++ terraform/metrics.tf
@@ -10,7 +10,51 @@ resource "helm_release" "vm" {
     value = "true"
   }
 
+  set {
+    name = "server.persistentVolume.existingClaim"
+    value = "vm"
+  }
+
   values = [templatefile("./prometheus.yml", { external_domain = var.external_domain, ping_id = var.ping_id })]
+}
+
+resource "kubernetes_persistent_volume_claim_v1" "vm" {
+  metadata {
+    name = "vm"
+  }
+  spec {
+    access_modes = [ "ReadWriteOnce" ]
+    storage_class_name = "generic"
+    volume_name = "vm"
+    resources {
+      requests = {
+        "storage" = "16Gi"
+      }
+    }
+  }
+}
+
+resource "kubernetes_persistent_volume_v1" "vm" {
+  metadata {
+    name = "vm"
+  }
+  spec {
+    access_modes = [ "ReadWriteOnce" ]
+    storage_class_name = "generic"
+    claim_ref {
+      name = "vm"
+    }
+    capacity = {
+      "storage" = "16Gi"
+    }
+    persistent_volume_source {
+      host_path {
+        path = "/storage/volume/pvc-ebf3d029-0eff-4c3c-9750-96f84a18be93_default_server-volume-vm-victoria-metrics-single-server-0"
+        type = "Directory"
+      }
+    }
+
+  }
 }
 
 
